2fdbb0804c94ce671c6826bb561738ec121e8b40
Carlos Henrique da Silva <brunacarlosilva@gmail.com>
2025-03-23 14:28:49 +0000

    Desafio Super Trunfo Nível Novato: Cadastro Básico Concluído

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,7 @@
 
 int main() {
     // Declaração das variáveis para os atributos das cartas
-    int populacao1, pontosTuristicos1, populacao2, pontosTuristicos2;
+    int populacao1, pontosturisticos1, populacao2, pontosturisticos2;
     float area1, pib1, area2, pib2;
 
     // Cadastro da primeira carta
@@ -14,7 +14,7 @@ int main() {
     printf("PIB (em bilhões): ");
     scanf("%f", &pib1);
     printf("Número de pontos turísticos: ");
-    scanf("%d", &pontosTuristicos1);
+    scanf("%d", &pontosturisticos1);
 
     // Cadastro da segunda carta
     printf("\nCadastro da segunda carta:\n");
@@ -25,7 +25,7 @@ int main() {
     printf("PIB (em bilhões): ");
     scanf("%f", &pib2);
     printf("Número de pontos turísticos: ");
-    scanf("%d", &pontosTuristicos2);
+    scanf("%d", &pontosturisticos2);
 
     // Exibição dos dados das cartas
 
@@ -34,14 +34,14 @@ int main() {
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões\n", pib1);
-    printf("Pontos turísticos: %d\n", pontosTuristicos1);
+    printf("Pontos turísticos: %d\n", pontosturisticos1);
 
     // segunda carta
     printf("\nDados da segunda carta:\n");
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões\n", pib2);
-    printf("Pontos turísticos: %d\n", pontosTuristicos2);
+    printf("Pontos turísticos: %d\n", pontosturisticos2);
 
     return 0;
 }
